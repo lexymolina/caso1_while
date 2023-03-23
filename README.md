bf191ceac719a0a03b8b338db9a96e993d8f7a65
sistemas <you@example.com>
2023-03-23 18:25:39 -0500

    se agrego diagrama de flujo y se modifico el caso1_while.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # caso1_while
 programa para hallar la suma de los primeros n numeros naturales
+
+#diagrama de flujo
+
+[diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ caso1_while.py
@@ -0,0 +1,12 @@
+# caso1 while
+
+#input
+n = int(input("digite un numero: "))
+
+suma = 0
+i = 1
+
+while i <= n:
+    suma = suma + i
+    i = i+1
+print("el resultado es:" ,suma)
